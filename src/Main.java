8aab383ad7870badc93f456a4509573af79e52a4
Yurii <shadowjura82@gmail.com>
2023-03-20 16:52:09 +0100

    Task2 has been completed

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,22 @@
 public class Main {
     public static void main(String[] args) {
-        task1();
+        task1_3();
     }
 
-    public static void task1() {
-        System.out.println("Задача 1");
+    public static void task1_3() {
+        System.out.println("Задачи 1-3");
+
+        // Задача 1
         var dog = 8.0;
         var cat = 3.6;
         var paper = 763789;
         System.out.println("Значение переменной dog - " + dog);
         System.out.println("Значение переменной cat - " + cat);
         System.out.println("Значение переменной paper - " + paper);
+
+        // Задача 2
+        dog = dog + 4;
+        cat = cat + 4;
+        paper = paper + 4;
     }
 }
