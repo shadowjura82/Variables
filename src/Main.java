57a655bd211c2626a63ca92e4cbf870c7c6dd129
Yurii <shadowjura82@gmail.com>
2023-03-20 17:33:29 +0100

    Задачи 4-5 выполнены

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,7 @@
 public class Main {
     public static void main(String[] args) {
         task1_3();
+        task4_5();
     }
 
     public static void task1_3() {
@@ -29,5 +30,26 @@ public class Main {
         System.out.println("Значение переменной dog - " + dog);
         System.out.println("Значение переменной cat - " + cat);
         System.out.println("Значение переменной paper - " + paper + "\n");
+    }
+
+    public static void task4_5() {
+        System.out.println("Задачи 4-5 \n");
+        // Задание 4
+        var friend = 19;
+        System.out.println("Значение переменной friend - " + friend);
+        friend = friend + 2;
+        System.out.println("Значение переменной friend + 2 - " + friend);
+        friend = friend / 7;
+        System.out.println("Значение переменной friend / 7 - " + friend + "\n");
+
+        //Задание 5
+        var frog = 3.5;
+        System.out.println("Значение переменной frog - " + frog);
+        frog *= 10;
+        System.out.println("Значение переменной frog + 10 - " + frog);
+        frog /= 3.5;
+        System.out.println("Значение переменной frog / 3.5 - " + frog);
+        frog += 4;
+        System.out.println("Значение переменной frog + 4 - " + frog);
     }
 }
